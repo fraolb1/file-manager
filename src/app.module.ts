24e13eba5841567f71e5e511e7d1345c8a23bac0
Firaol Bogale <firaolbogala98@gmail.com>
2024-03-03 11:39:07 +0300

    added env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { File } from './Model/file.model';
 import { FileService } from './services/file.service';
 import { FileController } from './controllers/file.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'iamdone',
-      database: 'fileManager',
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [File],
       synchronize: true,
     }),
